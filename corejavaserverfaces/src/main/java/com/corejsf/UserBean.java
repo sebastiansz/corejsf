a565743a5404e68b5a4200dd1e3344d3316d08bb
sebastian <sebastian@aps27662-mb>
2013-03-13 15:23:09 +0100

    na master 3

--- corejavaserverfaces/src/main/java/com/corejsf/UserBean.java
+++ corejavaserverfaces/src/main/java/com/corejsf/UserBean.java
@@ -2,30 +2,34 @@ package com.corejsf;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 // or import javax.faces.bean.ManagedBean;
-import javax.enterprise.context.SessionScoped;
+
 // or import javax.faces.bean.SessionScoped;
 
-@Named("user") // or @ManagedBean(name="user")
+@Named("user")
+// or @ManagedBean(name="user")
 @SessionScoped
 public class UserBean implements Serializable {
-    private String name;
-    private String password;
 
-    public String getName() {
-        return name;
-    }
+	private String name;
 
-    public void setName(String newValue) {
-        name = newValue;
-    }
+	private String password;
 
-    public String getPassword() {
-        return password;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setPassword(String newValue) {
-        password = newValue;
-    }
+	public void setName(String newValue) {
+		name = newValue;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String newValue) {
+		password = newValue;
+	}
 }
